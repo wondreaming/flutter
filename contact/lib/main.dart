0bbb52b803300ea75e125b5785f53596c92c1020
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-05-28 17:41:31 +0900

    stateful widget

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -6,125 +6,52 @@ void main() {
   runApp(const MyApp());
 } // runApp 앱을 시작해주세요
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  List<String> names = ['모모', '치치', '심상익'];
+  List<int> likes = [0, 0, 0];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // 구글 스타일, 커스터마이징  : MaterialApp, 아이폰 스타일 : Cupertino
       home: SafeArea(
         child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Text(a.toString()),
+            onPressed: () {
+              setState(() {
+                a++;
+              });
+              print(a);
+            },
+          ),
           appBar: AppBar(
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(8.0),
-              child: Container(
-                color: Colors.grey.withOpacity(0.3),
-                height: 2.0,
-              ),
-            ),
-            title: Text(
-              '금호동3가',
-              style: TextStyle(fontWeight: FontWeight.w700),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.search),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.menu),
-              ),
-              Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.notifications_none_rounded),
-                  ),
-                  Positioned(
-                    right: 10.0,
-                    top: 10.0,
-                    child: Container(
-                      width: 7.0,
-                      height: 7.0,
-                      decoration: BoxDecoration(
-                        color: CupertinoColors.activeOrange,
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ],
+            title: Text('TEST'),
+            backgroundColor: Colors.blue,
           ),
-          body: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                height: 160,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.grey.withOpacity(0.3),
-                    ),
+          body: ListView.builder(
+              itemCount: names.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  leading: Text(likes[i].toString()),
+                  title: Text(names[i]),
+                  trailing: ElevatedButton(
+                    child: Text('likes'),
+                    onPressed: (){
+                      setState(() {
+                        likes[i]++;
+                      });
+                    },
                   ),
-                ),
-                margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: Row(
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: ClipRRect(
-                        child: Image.asset('assets/chichi.JPEG'),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                    ),
-                    Flexible(
-                      fit: FlexFit.tight,
-                      flex: 2,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '캐논 DSLR 100D (단렌즈,\n충전기 16기가SD 포함)',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 18.0),
-                            ),
-                            Text(
-                              '성동구 행당동 * 끌올 10분전',
-                              style: TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            Text(
-                              '210,000원',
-                              style: TextStyle(
-                                  fontSize: 18.0, fontWeight: FontWeight.w600),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Icon(
-                                  Icons.favorite_outline,
-                                  color: Colors.grey.withOpacity(0.7),
-                                ),
-                                Text(' 4')
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                );
+              }),
         ),
       ),
     );
